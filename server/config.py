7da67ed9eed1f1449f183698beb184c8bdbc59fe
rajsh <rajshah9172@gmail.com>
2021-03-15 01:01:03 +0530

    added create company

--- server/config.py
+++ server/config.py
@@ -7,5 +7,7 @@ Created on Tue Jun 23 17:37:40 2020
 
 import os
 import sys
+
 os.chdir(os.getcwd())
-print('Current Working Directory', os.getcwd())
+print("Current Working Directory", os.getcwd())
+# print(os.getcwd() + "\\rew")
