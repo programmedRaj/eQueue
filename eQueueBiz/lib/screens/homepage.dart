e3dfa5b89795c2d6c21f41eb720297dff1b681a9
rushabh <rushabhm75@gmail.com>
2021-04-30 23:07:06 +0530

    go

--- eQueueBiz/lib/screens/homepage.dart
+++ eQueueBiz/lib/screens/homepage.dart
@@ -45,7 +45,7 @@ class _HomePageState extends State<HomePage> {
       builder: (context, bizdets, child) {
         return Consumer<AuthProv>(
           builder: (context, authProv, child) {
-            print(authProv.authinfo.companyType);
+            // print(authProv.authinfo.companyType);
             Provider.of<BookingBranDet>(context)
                 .getbookdets(authProv.authinfo.jwtToken);
             return authProv.authinfo.userType == null
